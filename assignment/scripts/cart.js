4550e1698f6354d34729b4e099f601ab37734b65
Rachel Bruce <rachelbruce922@gmail.com>
2022-05-26 20:29:39 -0500

    finished addItem. Started on listItems

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,24 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = []
+
+
+//addItem
+function addItem (item) {
+    basket.push(item);
+    return true
+}//end addItem
+
+console.log(addItem('item'));
+console.log('Basket:' , basket);
+console.log(addItem('anotherItem'));
+console.log('Basket:', basket);
+
+//listItems
+function listItems() {
+    for (let i=0; i < basket.length; i++) {
+        console.log(basket[i]);
+    }
+}
+// console.log(listItems())
