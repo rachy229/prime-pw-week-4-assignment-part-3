8fca88da817a6f8f8a3c8f8a11e922f3b28a97d1
Rachel Bruce <rachelbruce922@gmail.com>
2022-05-28 18:06:27 -0500

    working on listItems and empty

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -14,11 +14,30 @@ console.log(addItem('item'));
 console.log('Basket:' , basket);
 console.log(addItem('anotherItem'));
 console.log('Basket:', basket);
+console.log(addItem('cheese'));
+console.log('Basket:' , basket);
 
 //listItems
 function listItems() {
     for (let i=0; i < basket.length; i++) {
         console.log(basket[i]);
     }
+    return true
+}//end listItems
+
+console.log(listItems())
+
+//empty
+
+let basketLength = basket.length; 
+console.log(basketLength);
+function empty() {
+    while (basketLength > 0) {
+        basket.pop;
+    }
 }
-// console.log(listItems())
+
+
+// console.log(empty);
+
+// console.log('Basket:', basketLength);
